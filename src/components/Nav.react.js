d6bb19f4c5859a36bd5a397f4d4623459eb01a7f
Jay Dimock <jdpickle@gmail.com>
2023-01-24 17:46:47 -0800

    making nav bar responsive for small screens

--- src/components/Nav.react.js
+++ src/components/Nav.react.js
@@ -14,6 +14,7 @@ import { FAQ, ABOUT } from "../constants/ViewConstants";
 import { ListViewSetterButton } from "./ListViewSetterButton.react";
 
 import {
+  Box,
   AppBar,
   Container,
   Toolbar,
@@ -35,6 +36,7 @@ export const Nav = () => {
   const resetView = useResetRecoilState(viewState);
   const resetSyncing = useResetRecoilState(syncingState);
   const [view, setView] = useRecoilState(viewState);
+  const connected = user.id && tokenInfo.access_token;
 
   const logout = () => {
     resetTokenInfo();
@@ -45,60 +47,105 @@ export const Nav = () => {
     resetSyncing();
   };
 
+  const faq = (
+    <Link component="button" onClick={() => setView(FAQ)}>
+      FAQ
+    </Link>
+  );
+
+  const manage = (
+    <ListViewSetterButton component="link">Manage</ListViewSetterButton>
+  );
+
+  const disconnect = (
+    <Link component="button" onClick={logout}>
+      Disconnect
+    </Link>
+  );
+
+  const home = (
+    <Link onClick={() => setView(ABOUT)} href="#">
+      Home
+    </Link>
+  );
+
+  const connect = (
+    <Link href={`${APP_API_BASE}/auth/login`}>Connect to Spotify</Link>
+  );
+
+  const loggedOutMenuItems = (
+    <Box sx={{ position: { xs: "static", sm: "absolute" }, right: 0 }}>
+      <Typography variant="subtitle2">
+        {view === FAQ ? home : faq}
+        {" | "}
+        {connect}
+      </Typography>
+    </Box>
+  );
+
+  const loggedInMenuItems = (
+    <Box sx={{ position: { xs: "static", sm: "absolute" }, right: 0 }}>
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{ display: "flex", alignItems: "center" }}
+      >
+        <Avatar
+          alt={user.display_name}
+          src={user.image_url}
+          sx={{ display: { xs: "none", sm: "block" } }}
+        />
+        <div>
+          <Typography sx={{ display: { xs: "none", sm: "block" } }}>
+            {user.display_name}
+          </Typography>
+          <Typography variant="subtitle2">
+            {view === FAQ ? manage : faq}
+            {" | "}
+            {disconnect}
+          </Typography>
+        </div>
+      </Stack>
+    </Box>
+  );
+
+  const branding = (
+    <Stack
+      direction="row"
+      sx={{ alignItems: "center", mt: { xs: 2, sm: 0 } }}
+      spacing={1}
+    >
+      <Avatar
+        alt="S-logo"
+        src={logo}
+        sx={{ display: { xs: "none", sm: "block" } }}
+      />
+      <Avatar
+        alt={user.display_name}
+        src={user.image_url}
+        sx={{ display: { xs: "block", sm: "none" } }}
+      />
+      <Typography variant="h5">Spotification</Typography>
+      {syncing && (
+        <Typography variant="subtitle2" ml={2}>
+          (syncing...)
+        </Typography>
+      )}
+    </Stack>
+  );
+
   return (
     <AppBar position="sticky" color="secondary">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <IconButton>
-            <img id="logo" src={logo} alt="logo" />
-          </IconButton>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            Spotification
-            {syncing && <Typography variant="subtitle2">syncing...</Typography>}
-          </Typography>
-
-          <>
-            {!user.id || !tokenInfo.access_token ? (
-              <Typography variant="subtitle2">
-                {view === FAQ ? (
-                  <Link onClick={() => setView(ABOUT)} href="#">
-                    Home
-                  </Link>
-                ) : (
-                  <Link onClick={() => setView(FAQ)} href="#">
-                    FAQ
-                  </Link>
-                )}
-                {" | "}
-                <Link href={`${APP_API_BASE}/auth/login`}>
-                  Connect to Spotify
-                </Link>
-              </Typography>
-            ) : (
-              <Stack direction="row" spacing={2}>
-                <Avatar alt={user.display_name} src={user.image_url} />
-                <div>
-                  <Typography>{user.display_name}</Typography>
-                  <Typography variant="subtitle2">
-                    {view !== FAQ && (
-                      <Link component="button" onClick={() => setView(FAQ)}>
-                        FAQ
-                      </Link>
-                    )}
-                    {view === FAQ && (
-                      <ListViewSetterButton component="link">
-                        Manage
-                      </ListViewSetterButton>
-                    )}
-                    {" | "}
-                    <Link component="button" onClick={logout}>
-                      Disconnect
-                    </Link>
-                  </Typography>
-                </div>
-              </Stack>
-            )}
-          </>
+          <Stack
+            width="100%"
+            spacing={1}
+            direction={{ xs: "column", sm: "row" }}
+          >
+            {branding}
+            {connected ? loggedInMenuItems : loggedOutMenuItems}
+          </Stack>
         </Toolbar>
       </Container>
       <div id="menu-divider"></div>
